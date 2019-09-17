30ce55370adde339a536fecf653e540d47b79652
Piotr Fidler <fidler.piotr@gmail.com>
2019-09-17 19:44:40 +0200

    ExpressJS routing

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var fs = require('fs')
+var app = express();
+var stringifyFile;
+
+app.use(bodyParser.json());
+
+
+
+
+app.get('/getNote', function(req, res) {
+    fs.readFile('./test.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        stringifyFile = data
+        res.send(data);
+    });
+});
+
+app.post('/updateNote/:note', function(req, res) {
+    stringifyFile += req.params.note;
+    fs.writeFile('./test.json', stringifyFile, function(err) {
+      if (err) throw err;
+      console.log('file updated');
+      res.send(stringifyFile);
+    });
+  });
+
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
