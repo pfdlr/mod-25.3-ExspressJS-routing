1d4d8f60e2bda169070fe11c72c2341efefb0792
Piotr Fidler <fidler.piotr@gmail.com>
2019-09-19 10:08:17 +0200

    add static files

--- server.js
+++ server.js
@@ -1,30 +1,23 @@
 var express = require('express');
-var bodyParser = require('body-parser')
-var fs = require('fs')
 var app = express();
-var stringifyFile;
 
-app.use(bodyParser.json());
+app.use(express.static('assets'));
 
-app.get('/getNote', function(req, res) {
-    fs.readFile('./test.json', 'utf8', function(err, data) {
-        if (err) throw err;
-        stringifyFile = data
-        res.send(data);
-    });
+app.get('/', function (req, res) {
+  res.sendFile('/index.html');
 });
 
-app.post('/updateNote/:note', function(req, res) {
-    stringifyFile += '\n' + req.params.note;
-    fs.writeFile('./test.json', stringifyFile, function(err) {
-      if (err) throw err;
-      console.log('file updated');
-      res.send(stringifyFile);
-    });
-  });
+app.get('/userform', function (req, res) {
+  const response = {
+      first_name: req.query.first_name,
+      last_name: req.query.last_name
+  };
+  res.json(response);
+});
 
-app.listen(3000);
+var server = app.listen(3000, 'localhost', function() {
+    var host = server.address().address;
+    var port = server.address().port;
 
-app.use(function (req, res, next) {
-    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
